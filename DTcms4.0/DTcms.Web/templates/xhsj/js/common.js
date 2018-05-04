278b828c9e8e68aa4dda082c1e663f4109eaf209
regenkin <kinfly@foxmail.com>
2018-05-04 17:16:22 +0800

    增加xhsj模板

---
+++ DTcms4.0/DTcms.Web/templates/xhsj/js/common.js
@@ -0,0 +1,9 @@
+﻿
+//搜索查询
+function SiteSearch(send_url, divTgs) {
+	var str = $.trim($(divTgs).val());
+	if (str.length > 0 && str != "输入关健字") {
+	    window.location.href = send_url + "?keyword=" + encodeURI($(divTgs).val());
+	}
+	return false;
+}
