951d8daec46bcd3691f30f8388d73b15803a2e95
regenkin <kinfly@foxmail.com>
2018-05-22 17:54:56 +0800

    webapi url

--- DTcms5.0/Source/DTcms.Web.MVC/plugins/kfcenter/Util/WebApiUrl.cs
+++ DTcms5.0/Source/DTcms.Web.MVC/plugins/kfcenter/Util/WebApiUrl.cs
@@ -7,7 +7,7 @@ namespace DTcms.Web.Mvc.Plugin.KfCenter.Util
     public class WebApiUrl
     {
         public const string API = "http://localhost:900";
-        public const string API = "http://webapi.kinfar.net";
+        //public const string API = "http://webapi.kinfar.net";
 
         #region KfCenter
         /// <summary>
@@ -15,22 +15,21 @@ namespace DTcms.Web.Mvc.Plugin.KfCenter.Util
         /// </summary>
         public const string API_KfCenter_KfActSet_GetPageData = API+"/api/KfCenter/KfActSet/GetPageData";
         /// <summary>
-        /// 
+        /// 判断账套是否存在
         /// </summary>
         public const string API_KfCenter_KfActSet_Exist = API + "/api/KfCenter/KfActSet/Exist";
         /// <summary>
-        /// 
+        /// 获取账套实体
         /// </summary>
         public const string API_KfCenter_KfActSet_GetEntity = API + "/api/KfCenter/KfActSet/GetEntity";
         /// <summary>
-        /// 
+        /// 账套信息保存
         /// </summary>
         public const string API_KfCenter_KfActSet_Save = API + "/api/KfCenter/KfActSet/Save";
         /// <summary>
-        /// 
+        /// 测试数据库连接
         /// </summary>
         public const string API_KfCenter_KfActSet_TestActSetConnection = API + "/api/KfCenter/KfActSet/TestActSetConnection";
-
         //账套组
         /// <summary>
         /// 
