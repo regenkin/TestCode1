85c340f1aba768a4455914dbdab8ac93d6708875
regenkin <kinfly@foxmail.com>
2018-11-29 09:33:39 +0800

    1

---
+++ DTcms4.0/DTcms.Web/util/LogHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DTcms.Web
+{
+    public class LogHelper
+    {
+        static LogHelper()
+        {
+            //log4net.Config.DOMConfigurator.Configure();
+            log4net.Config.DOMConfigurator.Configure(new System.IO.FileInfo(System.AppDomain.CurrentDomain.BaseDirectory + "\\xmlconfig\\Log4net.config"));
+        }
+        public static readonly log4net.ILog loginfo = log4net.LogManager.GetLogger("loginfo");
+        public static readonly log4net.ILog logerror = log4net.LogManager.GetLogger("logerror");
+        public static readonly log4net.ILog logDebug = log4net.LogManager.GetLogger("logdebug");
+    }
+}
